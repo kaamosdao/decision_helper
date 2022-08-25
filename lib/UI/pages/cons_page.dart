20206c215f8d142acd81a6f806bf10d40b836275
kaamosdao <kaamosdao@gmail.com>
2022-08-25 18:42:12 +0300

    add ui on cons and pros pages

--- lib/UI/pages/cons_page.dart
+++ lib/UI/pages/cons_page.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ConsPage extends StatelessWidget {
+class ConsPage extends StatefulWidget {
   const ConsPage({Key? key}) : super(key: key);
 
   @override
+  State<ConsPage> createState() => _ConsPageState();
+}
+
+class _ConsPageState extends State<ConsPage> {
+  final consController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cons'),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 150, left: 50, right: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('add new cons'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                    child: TextFormField(
+                      onFieldSubmitted: (value) {
+                        final isValidForm = _formKey.currentState!.validate();
+                        if (isValidForm) {
+                          debugPrint('Add cons: $value');
+                        }
+                      },
+                      autofocus: true,
+                      controller: consController,
+                      maxLength: 50,
+                      decoration: const InputDecoration(
+                        labelText: 'Cons',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) =>
+                          value == '' ? 'Should not be empty' : null,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Cons $index'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/UI/pages/pros_page.dart
+++ lib/UI/pages/pros_page.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ProsPage extends StatelessWidget {
+class ProsPage extends StatefulWidget {
   const ProsPage({Key? key}) : super(key: key);
 
   @override
+  State<ProsPage> createState() => _ProsPageState();
+}
+
+class _ProsPageState extends State<ProsPage> {
+  final prosController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Pros'),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 150, left: 50, right: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('add new pros'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                    child: TextFormField(
+                      onFieldSubmitted: (value) {
+                        final isValidForm = _formKey.currentState!.validate();
+                        if (isValidForm) {
+                          debugPrint('Add pros: $value');
+                        }
+                      },
+                      autofocus: true,
+                      controller: prosController,
+                      maxLength: 50,
+                      decoration: const InputDecoration(
+                        labelText: 'Pros',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) =>
+                          value == '' ? 'Should not be empty' : null,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Pros $index'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
