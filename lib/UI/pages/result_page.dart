eef6183fa6db3e9a8d26917d29be93f5969f2ce3
kaamosdao <kaamosdao@gmail.com>
2022-09-13 16:36:35 +0300

    add diagramm to result

--- lib/UI/pages/result_page.dart
+++ lib/UI/pages/result_page.dart
@@ -1,6 +1,8 @@
 import 'package:decision_helper/bloc/decision_bloc.dart';
+import 'package:decision_helper/models/prosandcons_data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -13,10 +15,55 @@ class ResultPage extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Center(
-        child: BlocBuilder<DecisionBloc, DecisionState>(
-          builder: (context, state) {
-            return Text(state.decision);
-          },
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height - 300,
+          child: BlocBuilder<DecisionBloc, DecisionState>(
+            builder: (context, state) {
+              List<ProsAndConsData> data = [
+                ProsAndConsData(
+                  name: 'Pros',
+                  count: state.pros.length,
+                  label: '+',
+                ),
+                ProsAndConsData(
+                  name: 'Cons',
+                  count: state.cons.length,
+                  label: '-',
+                ),
+              ];
+              return SfCircularChart(
+                title: ChartTitle(text: state.decision),
+                legend: Legend(
+                  isVisible: true,
+                  position: LegendPosition.bottom,
+                  iconHeight: 18,
+                  textStyle: const TextStyle(fontSize: 13),
+                  itemPadding: 36,
+                ),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <DoughnutSeries<ProsAndConsData, String>>[
+                  DoughnutSeries<ProsAndConsData, String>(
+                    dataSource: data,
+                    xValueMapper: (ProsAndConsData data, _) => data.name,
+                    yValueMapper: (ProsAndConsData data, _) => data.count,
+                    dataLabelMapper: (ProsAndConsData data, _) => data.label,
+                    dataLabelSettings: const DataLabelSettings(
+                      isVisible: true,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                    enableTooltip: true,
+                  ),
+                ],
+                palette: const <Color>[
+                  Color.fromARGB(255, 69, 179, 96),
+                  Color.fromARGB(255, 236, 93, 93),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
---
+++ lib/models/prosandcons_data_model.dart
@@ -0,0 +1,10 @@
+class ProsAndConsData {
+  ProsAndConsData({
+    required this.name,
+    required this.count,
+    required this.label,
+  });
+  final String name;
+  final num count;
+  final String label;
+}
